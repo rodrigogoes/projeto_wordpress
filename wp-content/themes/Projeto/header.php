74933c0b3db45dbcdefea7da22d9ac9ea4b81526
rodrigogoes <rodriguinho-1993@hotmail.com>
2018-08-23 19:49:34 -0300

    Cards

--- wp-content/themes/Projeto/header.php
+++ wp-content/themes/Projeto/header.php
@@ -13,7 +13,7 @@
         </header>
 
 
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark" role="navigation">
   <div class="container">
 	<!-- Brand and toggle get grouped for better mobile display -->
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
--- wp-content/themes/Projeto/index.php
+++ wp-content/themes/Projeto/index.php
@@ -1,13 +1,15 @@
 <?php // CÓDIGO LATERAL ?>
 
 <?php get_header(); ?>
-<div class="container">
+<div class="container" style="width: 1500px">
+
 
 
  <?php // IMPRIMIR POSTS  ?>
    
+              <div class='row' style="margin-right: 15px">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-              echo "<div class='card'>
+              echo "<div class='card' style='margin-right: 5px; margin-top: 5px' >
               <div class='card-header bg-dark text-white'>";?>
         
         <h1>
@@ -23,13 +25,17 @@
         
         <p><?php comments_number('0 Comentários', '1 Comentário', '% Comentários' );?></p>
        
-       <?php the_post_thumbnail( array( '500, 500') );   ?>
+       <?php the_post_thumbnail( array( '500, 500') ); 
+         ?>
+       </div>
+     </div>
+
         
         <?php endwhile; else : ?>
         <p><?php _e( 'Nenhum post encontrado!' ); ?></p>
         <?php endif; ?>
 
-
+</div>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
